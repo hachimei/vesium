8fc2141149f86caedbe5cc4c6c80daaeeaf97987
胡贵涛 <hugt@mapout.net>
2019-08-27 18:01:23 +0800

    add EventManager;add unit test";modify tsconfig

--- src/core/EventManager.ts
+++ src/core/EventManager.ts
@@ -92,9 +92,188 @@ export default class EventManager {
     onMouseLeave: ScreenSpaceEventType.MOUSE_MOVE,
   }
 
-  /* private scene: Cesium.Scene;
+  private scene: Cesium.Scene
   private sshe: ScreenSpaceEventHandler
   private events: EventMap<Map<any, Callback>> = {
+    onClick: new Map(),
+    onDoubleClick: new Map(),
+    onMouseDown: new Map(),
+    onMouseUp: new Map(),
+    onMiddleClick: new Map(),
+    onMiddleDown: new Map(),
+    onMiddleUp: new Map(),
+    onMouseMove: new Map(),
+    onPinchEnd: new Map(),
+    onPinchMove: new Map(),
+    onPinchStart: new Map(),
+    onRightClick: new Map(),
+    onRightDown: new Map(),
+    onRightUp: new Map(),
+    onWheel: new Map(),
+    onMouseEnter: new Map(),
+    onMouseLeave: new Map(),
+  }
+  private hovered = new Map<any, boolean>()
+  private changed = new Map<any, boolean>()
 
-  } */
+  public constructor(scene: Cesium.Scene, canvas: HTMLCanvasElement) {
+    this.scene = scene
+    this.sshe = new ScreenSpaceEventHandler(canvas)
+  }
+
+  public destroy() {
+    if (!this.sshe.isDestroyed) {
+      this.sshe.destroy()
+    }
+  }
+
+  public isDestroyed() {
+    return this.sshe.isDestroyed()
+  }
+
+  public on(element: any, type: EventType, cb: Callback) {
+    this.events[type].set(element, cb)
+  }
+
+  public off(element: any, type: EventType) {
+    this.events[type].delete(element)
+  }
+
+  public setEvents(element: any, props: object) {
+    Object.entries(props).forEach(([k, v]) => {
+      const et = k as EventType
+      if (eventNames.includes(et)) {
+        if (v) {
+          this.on(element, et, v)
+        } else {
+          this.off(element, et)
+        }
+      }
+    })
+    this.commit()
+  }
+
+  public clearEvents(element: any) {
+    eventNames.forEach(et => {
+      this.off(element, et)
+    })
+    this.commit()
+  }
+
+  public commit() {
+    const sshe = this.sshe
+    const destroyed = this.sshe.isDestroyed()
+    const elements = new Set(this.hovered.keys())
+    const elements2 = new Set<any>()
+
+    if (!destroyed) {
+      if (
+        this.events.onMouseEnter.size === 0 &&
+        this.events.onMouseLeave.size === 0 &&
+        this.events.onMouseMove.size === 0
+      ) {
+        this.sshe.removeInputAction(ScreenSpaceEventType.MOUSE_MOVE)
+      } else if (!this.sshe.getInputAction(ScreenSpaceEventType.MOUSE_MOVE)) {
+        this.sshe.setInputAction(this.onMouseMove as any, ScreenSpaceEventType.MOUSE_MOVE)
+      }
+    }
+
+    Object.entries(this.events).forEach(([et, m]) => {
+      const eventType = et as EventType
+
+      m.forEach((v, k) => {
+        if (!this.hovered.has(k)) {
+          this.hovered.set(k, false)
+        }
+        elements2.add(k)
+      })
+
+      if (et === 'onMouseEnter' || et === 'onMouseLeave' || et === 'onMouseMove') {
+        return
+      }
+
+      const cesiumEventType = EventManager.eventTypeMap[eventType]
+
+      if (!destroyed) {
+        if (m.size === 0) {
+          sshe.removeInputAction(cesiumEventType)
+        } else if (!sshe.getInputAction(cesiumEventType)) {
+          sshe.setInputAction(this.eventCallback(eventType) as any, cesiumEventType)
+        }
+      }
+
+      elements.forEach(e => {
+        if (!elements2.has(e)) {
+          this.hovered.delete(e)
+        }
+      })
+    })
+  }
+
+  public getScreenSpaceEventHandler() {
+    return this.sshe
+  }
+
+  private eventCallback = (et: EventType) => (e: CesiumMovementEvent) => {
+    const picked = this.pick(e.position)
+
+    if (picked) {
+      this.events[et].forEach((cb, element) => {
+        if (pickedObjectEquals(picked, element)) {
+          cb(e, element)
+        }
+      })
+    }
+  }
+
+  private onMouseMove = (e: CesiumMovementEvent) => {
+    const picked = this.pick(e.endPosition)
+    this.changed.clear()
+
+    this.hovered.forEach((h, element) => {
+      const p = pickedObjectEquals(picked, element)
+      this.hovered.set(element, p)
+      if (p !== h) {
+        this.changed.set(element, p)
+      }
+    })
+
+    if (picked) {
+      this.events.onMouseMove.forEach((cb, element) => {
+        if (this.hovered.get(element)) {
+          cb(e, element)
+        }
+      })
+    }
+
+    this.changed.forEach((hovered, element) => {
+      if (hovered) {
+        const onMouseEnter = this.events.onMouseEnter.get(element)
+        if (onMouseEnter) {
+          onMouseEnter(e, element)
+        }
+      } else {
+        const onMouseLeave = this.events.onMouseLeave.get(element)
+        if (onMouseLeave) {
+          onMouseLeave(e, element)
+        }
+      }
+    })
+  }
+
+  private pick(pos?: Cesium.Cartesian2): any | undefined {
+    if (!pos) {
+      return undefined
+    }
+    const picked = this.scene.pick(pos)
+    if (picked) {
+      // Entity
+      if (picked.id instanceof Cesium.Entity) {
+        return picked.id
+      }
+      // Other
+      return picked
+    }
+    return undefined
+  }
 }
---
+++ src/core/__tests__/EventManager.test.ts
@@ -0,0 +1,56 @@
+import { ScreenSpaceEventType } from 'cesium'
+
+import EventManager from '../EventManager'
+
+describe('core/EventManager', () => {
+  const element = 'element'
+  const fn = () => () => {}
+
+  it('should attach and dettach event', () => {
+    const em: EventManager = new (EventManager as any)()
+    const sseh = em.getScreenSpaceEventHandler()
+
+    expect(sseh.getInputAction(ScreenSpaceEventType.LEFT_CLICK)).toBe(undefined)
+    expect(sseh.getInputAction(ScreenSpaceEventType.MOUSE_MOVE)).toBe(undefined)
+
+    em.on(element, 'onClick', fn())
+    em.on(element, 'onMouseEnter', fn())
+    em.commit()
+
+    expect(typeof sseh.getInputAction(ScreenSpaceEventType.LEFT_CLICK)).toBe('function')
+    expect(typeof sseh.getInputAction(ScreenSpaceEventType.MOUSE_MOVE)).toBe('function')
+  })
+
+  it('should update events', () => {
+    const em: EventManager = new (EventManager as any)()
+    const sseh = em.getScreenSpaceEventHandler()
+
+    em.setEvents(element, { onClick: fn() })
+    expect(typeof sseh.getInputAction(ScreenSpaceEventType.LEFT_CLICK)).toBe('function')
+    em.setEvents(element, { onClick: undefined })
+    expect(sseh.getInputAction(ScreenSpaceEventType.LEFT_CLICK)).toBe(undefined)
+  })
+
+  it('should clear events', () => {
+    const em: EventManager = new (EventManager as any)()
+    const sseh = em.getScreenSpaceEventHandler()
+
+    em.setEvents(element, { onClick: fn() })
+    expect(typeof sseh.getInputAction(ScreenSpaceEventType.LEFT_CLICK)).toBe('function')
+    em.clearEvents(element)
+    expect(sseh.getInputAction(ScreenSpaceEventType.LEFT_CLICK)).toBe(undefined)
+  })
+
+  it('should destroy', () => {
+    const em: EventManager = new (EventManager as any)()
+    const sseh = em.getScreenSpaceEventHandler()
+
+    expect(em.isDestroyed()).toBe(false)
+    expect(sseh.isDestroyed()).toBe(false)
+
+    em.destroy()
+
+    expect(em.isDestroyed()).toBe(true)
+    expect(sseh.isDestroyed()).toBe(true)
+  })
+})
